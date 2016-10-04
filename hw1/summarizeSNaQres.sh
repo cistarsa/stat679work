bd6fee1eacd6e0147e9d69152b68ed8010a35d69
kingcohn1 <zachary.pcohen@gmail.com>
2016-10-04 17:51:11 -0500

    Create summarizeSNaQres.sh

---
+++ hw1/summarizeSNaQres.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+exec >out.csv
+
+
+### At first get hmax from each file
+for i in {1..9};do 
+grep "hmax" log/timetest$0{i}_snaq.log 
+grep "hmax" out/timetest$0{i}_snaq.out	
+
+
+### print the CPU time 
+ps -e -o %cpu | awk '{s+=$1} END {print s}' 
+
+done

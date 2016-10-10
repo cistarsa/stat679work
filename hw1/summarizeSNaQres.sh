4eeeb48967999907650737856184646f252c48b7
kingcohn1 <zachary.pcohen@gmail.com>
2016-10-10 10:12:42 -0500

    Update summarizeSNaQres.sh

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-echo analysis,Hmax,CPU > out6.csv
-for i in log2/*.log;
+echo analysis,Hmax,cputime > out5.csv
+for i in log/*.log;
 do
-        echo "i=$i"
-        analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
-        h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
-        CPU=$(grep -E "Elapsed time. [[:digit:]]+" -o out/$analysis.out)
-        echo "analysis=$analysis ; h=$h ; CPU=$CPU"
-    echo "$analysis,$h,$CPU" >> out6.csv
+	echo "i=$i" 
+	analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
+	h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
+	CPU=$(grep -E "Elapsed time. [[:digit:]]+" -o out/$analysis.out | grep -E -o "[[:digit:]]+")	
+	echo "h=$h ; analysis=$analysis ; CPU=$CPU"
+    echo "$analysis,$h,$CPU" >> out5.csv	
 done

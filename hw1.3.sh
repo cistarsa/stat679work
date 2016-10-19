d13a520a82a5a9f7d2a31c7935abfa1fcba2fa24
kingcohn1 <zachary.pcohen@gmail.com>
2016-10-19 12:14:42 -0500

    Update hw1.3.sh

--- hw1.3.sh
+++ hw1.3.sh
@@ -16,17 +16,20 @@ do
         xabs=$(grep -E "xtolAbs=[[:digit:]].+ " -o $i | head -n1 | egrep -o "[[:digit:]]+.[[:digit:]]+") # grep used for xtolAbs number, and nothing more
 	xrel=$(grep -E "xtolRel=[[:digit:]].+" -o $i | head -n1 | egrep -o "[[:digit:]]+.[[:digit:]]+") # grep to xtolRel values
 	seed=$(grep -E "seed [[:digit:]]+ " -o $i | head -n1 | egrep -o "[[:digit:]]+") # grep to get main seed number
+	
+echo $analysis,$Nruns,$Nfail,$fabs,$frel,$xabs,$xrel,$seed 
 
 l=$(sed -nE 's/.*-loglik=([0-9,]+).*/\1/p' $i) # sed again is used isolate the loglik values in my files that match initial $i variable, which are then piped into if statements. Here, sed is better than grep as it is able to read the files and execute commands simultaneously. As opposed to grep. 
 a=0
 b=0
-c-0
+c=0
 for number in $l # for look to recall my $l variable from the sed command
 	do 
 	 if [ $number -lt 3460 ] #first if loop to count the number of loglik values less than 3460
          then
          a=$((a+1)) #variable is 0+1 for every time it is less than 3460, same syntax for values less than 3450, 3440.
 	fi
+echo $l,$a
 	if [ $number -lt 3450 ]
 	 then
 	 b=$((b+1))
